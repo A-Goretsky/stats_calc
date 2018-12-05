e5eefb2dc8368135a9b3afafc063bd22b5e383e0
Anton Goretsky <anton.goretsky@gmail.com>
2018-12-04 21:43:59 -0500

    add beginning

---
+++ basic_calc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void mean(double[] list) {
+	double sum;
+	for (int x = 0; x < (int) list.size(), x++) {
+		sum += list[x];
+	}
+	double res = sum / (int) list.size();
+	std::cout << res << "\n";
+}
+
+void freq_list(double[] list) {
+	std::cout << "Not Written Yet\n";
+}
+
+void freq_chart(double[] list) {
+	std::cout << "Not Written Yet\n";
+}
