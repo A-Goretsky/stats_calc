bf5023ca0a41d3af138dcbe76a52f258fdefed91
Anton Goretsky <anton.goretsky@gmail.com>
2019-11-01 20:30:02 -0400

    shell made for next step

--- machine_learning/general.cpp
+++ machine_learning/general.cpp
@@ -10,31 +10,57 @@ class Numeric_Instance {
         Numeric_Instance() = default;
         Numeric_Instance(std::vector<double> input) {
             features = input;
-            feature_count = input.size();
+            feature_count = input.size() - 1; //exclude label
+            label = input[feature_count]; //label = last element
+            input.pop_back(); //Remove class label from feature vector
+        }
+        //Copy Constructor
+        Numeric_Instance(const Numeric_Instance& other) {
+
+        }
+        //Move Constructor
+        Numeric_Instance(Numeric_Instance&& other) {
+            
+        }
+        friend std::ostream& operator<<(std::ostream& out, Numeric_Instance input) {
+            return out;
+        }
+        //Copy assignment
+        Numeric_Instance& operator=(const Numeric_Instance& other) {
+            
+        }
+        //Move assignment
+        Numeric_Instance& operator=(Numeric_Instance&& other) {
+
         }
     private:
         std::vector<double> features;
         size_t feature_count;
-        std::string label; //class
+        std::string label = ""; //class
 };
 
 class Data {
     public:
+        //Default constructor
         Data() = default;
+        //Constructor using vectored numeric input.
+        //Mostly for the purpose of testing
         Data(std::vector<std::vector<double>> input) {
             instance_count = input.size();
             size_t features = input[0].size();
             for (size_t x = 0; x < instance_count; x++) {
                 for (size_t y = 0; y < features; y++) {
-                    instances.
+                    Numeric_Instance* to_add = new Numeric_Instance;
+                    instances.push_back(to_add);
                 }
             }
         }
-        Data(std::string filename) {
-
+        //Constructor for .csv and .arff files
+        Data(std::string filename, std::string type) {
+            
         }
     private:
-        std::vector<Numeric_Instance> instances;
+        std::vector<Numeric_Instance *> instances;
         size_t instance_count;
 };
 
